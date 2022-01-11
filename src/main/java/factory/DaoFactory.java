0968b29cba43bcae85f2bc5777e81263fa5432f9
braiso-22 <braisfv22@gmail.com>
2022-01-11 20:23:41 +0100

    DaoFactory creado y metodo para alumno

---
+++ src/main/java/factory/DaoFactory.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package factory;
+
+import dao.AlumnoDAO;
+
+/**
+ *
+ * @author brais.fernandezvazqu
+ */
+public class DaoFactory {
+
+    public DaoFactory() {
+        super();
+    }
+
+    public AlumnoDAO getAlumnoDao() {
+        return new AlumnoDAO();
+    }
+
+}
