eb6dd3f21e190bb65b2845aeb02b0c52399b06b8
braiso-22 <braisfv22@gmail.com>
2022-01-11 20:23:16 +0100

    alumno modificado

--- src/main/java/vo/Alumno.java
+++ src/main/java/vo/Alumno.java
@@ -2,6 +2,7 @@ package vo;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.Objects;
 import javax.persistence.*;
 
 /**
@@ -9,20 +10,109 @@ import javax.persistence.*;
  * @author brais.fernandezvazqu
  */
 @Entity
-@Table(name = "STUDENT")
+@Table(name = "Alumno")
 public class Alumno implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String codigo;
-    @Column(name = "dni",nullable = false)
+    private String id;
+    @Column(name = "dni", nullable = false)
     private String dni;
-    @Column(name = "nombre",nullable = false)
+    @Column(name = "nombre", nullable = false)
     private String nombre;
-    @Column(name = "apellidos",nullable = false)
+    @Column(name = "apellidos", nullable = false)
     private String apellidos;
-    @Column(name = "curso",nullable = false)
+    @Column(name = "curso", nullable = false)
     private String curso;
-    @Column(name = "fecha_nacimiento",nullable = false)
+    @Column(name = "fecha_nacimiento", nullable = false)
     private LocalDate fechaNacimiento;
+
+    public Alumno() {
+        super();
+    }
+
+    public Alumno(String id, String dni, String nombre, String apellidos, String curso, LocalDate fechaNacimiento) {
+        super();
+        this.id = id;
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.curso = curso;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getCodigo() {
+        return id;
+    }
+
+    public void setCodigo(String codigo) {
+        this.id = codigo;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Alumno other = (Alumno) obj;
+        return Objects.equals(this.id, other.id);
+    }
+
+    @Override
+    public String toString() {
+        return "id:" + id + ", dni:" + dni + ", nombre:" + nombre + ", apellidos:" + apellidos + ", curso:" + curso + ", fechaNacimiento:" + fechaNacimiento + "\n";
+    }
+
 }
