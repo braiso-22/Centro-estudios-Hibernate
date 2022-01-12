a8af3421afb24bf02827c7c0e01e50a662d59741
braiso-22 <braisfv22@gmail.com>
2022-01-12 20:24:59 +0100

    Profesor creado

---
+++ src/main/java/vo/Profesor.java
@@ -0,0 +1,161 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vo;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author brais.fernandezvazqu
+ */
+@Entity
+@Table(name = "Profesor")
+public class Profesor implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Column(name = "dni", nullable = false)
+    private String dni;
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+    @Column(name = "apellidos", nullable = false)
+    private String apellidos;
+    @Column(name = "departamento", nullable = false)
+    private String departamento;
+    @Column(name = "sueldo", nullable = false)
+    private float sueldo;
+
+    public Profesor() {
+    }
+
+    public Profesor(String dni, String nombre, String apellidos, String departamento, float sueldo) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.departamento = departamento;
+        this.sueldo = sueldo;
+    }
+
+    public Profesor(String id, String dni, String nombre, String apellidos, String departamento, float sueldo) {
+        this.id = id;
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.departamento = departamento;
+        this.sueldo = sueldo;
+    }
+
+    /**
+     * Get the value of sueldo
+     *
+     * @return the value of sueldo
+     */
+    public float getSueldo() {
+        return sueldo;
+    }
+
+    /**
+     * Set the value of sueldo
+     *
+     * @param sueldo new value of sueldo
+     */
+    public void setSueldo(float sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    /**
+     * Get the value of departamento
+     *
+     * @return the value of departamento
+     */
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    /**
+     * Set the value of departamento
+     *
+     * @param departamento new value of departamento
+     */
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    /**
+     * Get the value of apellidos
+     *
+     * @return the value of apellidos
+     */
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    /**
+     * Set the value of apellidos
+     *
+     * @param apellidos new value of apellidos
+     */
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    /**
+     * Get the value of nombre
+     *
+     * @return the value of nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Set the value of nombre
+     *
+     * @param nombre new value of nombre
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * Get the value of dni
+     *
+     * @return the value of dni
+     */
+    public String getDni() {
+        return dni;
+    }
+
+    /**
+     * Set the value of dni
+     *
+     * @param dni new value of dni
+     */
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "id:" + id + ", dni:" + dni + ", nombre:" + nombre + ", apellidos:" + apellidos + ", departamento=" + departamento + ", sueldo=" + sueldo + "\n";
+    }
+
+}
